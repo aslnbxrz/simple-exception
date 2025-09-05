d62d1f5f99bfb8d70225a278c39fd70981ab062d
aslnbxrz <bexruz.aslonov1@gmail.com>
2025-09-05 18:05:32 +0500

    v1.0.24: Version bump (patch)

--- src/Traits/HasStatusCode.php
+++ src/Traits/HasStatusCode.php
@@ -14,21 +14,4 @@ trait HasStatusCode
         return $this->value;
     }
 
-    /**
-     * Get the HTTP status code for the enum case.
-     * Auto-maps based on status code value.
-     */
-    public function httpStatusCode(): int
-    {
-        $code = $this->value;
-        
-        // Map common status codes
-        return match (true) {
-            $code >= 200 && $code < 300 => Response::HTTP_OK,
-            $code >= 300 && $code < 400 => Response::HTTP_MOVED_PERMANENTLY,
-            $code >= 400 && $code < 500 => Response::HTTP_BAD_REQUEST,
-            $code >= 500 && $code < 600 => Response::HTTP_INTERNAL_SERVER_ERROR,
-            default => Response::HTTP_INTERNAL_SERVER_ERROR
-        };
-    }
 }
