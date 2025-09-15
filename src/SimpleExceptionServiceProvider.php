71d9b809218c7b55bb2e3f06f7b6ac873f609c47
aslnbxrz <bexruz.aslonov1@gmail.com>
2025-09-15 12:39:45 +0500

    v1.2.2: Version bump (patch)

--- src/SimpleExceptionServiceProvider.php
+++ src/SimpleExceptionServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace Aslnbxrz\SimpleException;
 
+use Aslnbxrz\SimpleException\Console\Commands\MakeErrorRespCodeCommand;
+use Aslnbxrz\SimpleException\Console\Commands\SyncTranslationsCommand;
 use Illuminate\Support\ServiceProvider;
 
 class SimpleExceptionServiceProvider extends ServiceProvider
@@ -10,15 +12,14 @@ class SimpleExceptionServiceProvider extends ServiceProvider
     {
         // Load helpers (fallback if not composer "files")
         $helpers = __DIR__ . '/Support/helpers.php';
-        if (file_exists($helpers)) {
+        if (is_file($helpers)) {
             require_once $helpers;
         }
 
-        // Merge/preset config
-        $path = __DIR__ . '/../config/simple-exception.php';
-
-        if (file_exists($path)) {
-            $this->mergeConfigFrom($path, 'simple-exception');
+        // 2) Config merge
+        $configPath = __DIR__ . '/../config/simple-exception.php';
+        if (is_file($configPath)) {
+            $this->mergeConfigFrom($configPath, 'simple-exception');
         } else {
             // Safe fallback so tests don't crash if config file is missing
             $defaults = [
@@ -42,34 +43,57 @@ class SimpleExceptionServiceProvider extends ServiceProvider
                 'enum_generation' => ['resp_codes_dir' => 'Enums/RespCodes'],
                 'translations' => ['base_path' => 'vendor/simple-exception'],
                 'performance' => ['cache_messages' => true, 'cache_duration' => 3600],
+                'messages' => [
+                    'locales' => [],
+                    'locale_fallback' => 'en',
+                    'patterns' => ['en' => ':readable error occurred.'],
+                    'overrides' => [],
+                ],
             ];
-
             $existing = $this->app['config']->get('simple-exception', []);
             $this->app['config']->set('simple-exception', array_replace_recursive($defaults, $existing));
         }
 
-        // (ixtiyoriy) Commandlarni registratsiya qiling:
+        // Console commands
         if ($this->app->runningInConsole()) {
             $this->commands([
-                \Aslnbxrz\SimpleException\Console\Commands\MakeErrorRespCodeCommand::class,
-                \Aslnbxrz\SimpleException\Console\Commands\SyncTranslationsCommand::class,
+                MakeErrorRespCodeCommand::class,
+                SyncTranslationsCommand::class,
             ]);
         }
     }
 
     public function boot(): void
     {
-        // Publish config for end-users
-        $this->publishes([
-            __DIR__ . '/../config/simple-exception.php' => config_path('simple-exception.php'),
-        ], 'config');
+        // Publish config
+        $configSrc = __DIR__ . '/../config/simple-exception.php';
+        if (is_file($configSrc)) {
+            $this->publishes([
+                $configSrc => config_path('simple-exception.php'),
+            ], 'simple-exception-config');
+        }
 
         // Publish stubs if you ship them
-        $this->publishes([
-            __DIR__ . '/Console/Commands/stubs/ErrorRespCode.stub' => base_path('stubs/ErrorRespCode.stub'),
-        ], 'stubs');
+        $stubSrc = __DIR__ . '/Console/Commands/stubs/ErrorRespCode.stub';
+        if (is_file($stubSrc)) {
+            $this->publishes([
+                $stubSrc => base_path('stubs/ErrorRespCode.stub'),
+            ], 'simple-exception-stubs');
+        }
 
-        // Load translations if you ship package langs
-        $this->loadTranslationsFrom(__DIR__ . '/../lang', 'simple-exception');
+        // Publish translations
+        $packageLangPath = __DIR__ . '/../resources/lang';
+        if (is_dir($packageLangPath)) {
+            $this->loadTranslationsFrom($packageLangPath, 'simple-exception');
+
+            // Publish target (Laravel 9+: lang/, old: resources/lang/)
+            $target = function_exists('lang_path')
+                ? lang_path('vendor/simple-exception')
+                : resource_path('lang/vendor/simple-exception');
+
+            $this->publishes([
+                $packageLangPath => $target,
+            ], 'simple-exception-translations');
+        }
     }
 }
